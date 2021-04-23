aec45377bf41afb9a29ec70ba2b5fae213bf36b1
Svitlana Seniuta <68549621+svitlnamaks@users.noreply.github.com>
2021-04-23 17:01:53 -0400

    Lesson9 Commit1

---
+++ Lesson9/task2.9.py
@@ -0,0 +1,16 @@
+# Task 2
+# Doggy age
+# Create a class Dog with class attribute `age_factor` equals to 7.
+# Make __init__() which takes values for a dog’s age.
+# Then create a method `human_age` which returns the dog’s age in human equivalent.
+class Dog :
+    def __init__(self, dog_age):
+        self.dog_age = dog_age
+
+    def human_age(self, age_factor=7):
+        self.human_age = self.dog_age * age_factor
+        print(f'The dog’s age in human equivalent is {self.human_age}')
+
+
+my_dog = Dog(float(input('Hove old is your dog ?\n')))
+my_dog.human_age()
