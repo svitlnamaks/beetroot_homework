32cb4e7676afead447e8929cbb5d47efd9d08853
Svitlana Seniuta <68549621+svitlnamaks@users.noreply.github.com>
2021-06-12 15:29:11 -0400

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,10 +3,11 @@
 # Make a list that contains all integers from 1 to 100,
 # then find all integers from the list that are divisible by 7 but not a multiple of 5,
 # and store them in a separate list. Finally, print the list.
-new_list1 = list(range(1, 101))
-num = 7
-new_list2 = [i / num for i in new_list1]
-new_list3 = new_list2[6:-3:7]
-del new_list3[4:5]
-del new_list3[8:9]
-print(new_list3)
+a=list(range(1,101))
+b=list()
+i=0
+while i<len(a):
+    if i%5!=0 and i%7==0:
+        b.append(i)
+    i=i+7
+print(b)
